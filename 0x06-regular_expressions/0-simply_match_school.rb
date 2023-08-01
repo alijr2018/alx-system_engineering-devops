42d600a35c768e14547aa740d9f9a66dc5e1c133
alijr2018 <alijr_2018@proton.me>
2023-08-01 21:59:34 +0100

    done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,3 @@
 #!/usr/bin/env ruby
 
-def match_school(arg)
-    # Define the regular expression pattern to match "School"
-    regex = /School/
-  
-    # Use the `=~` operator to check if the argument contains the pattern
-    if arg =~ regex
-      puts arg.gsub(regex, 'School') # Replace matched "School" with "School"
-    else
-      puts "$" # If the pattern is not found, print "$"
-    end
-  end
-  
-  # Accept one argument from the command line
-  if ARGV.empty?
-    puts "Usage: #{$PROGRAM_NAME} <string>"
-  else
-    match_school(ARGV[0])
-  end
+puts ARGV[0].scan(/School/).join
