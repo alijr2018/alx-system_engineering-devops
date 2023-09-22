103fedc6c4f268b73f1abfd90da53e4886baba72
alijr2018 <alirami120@hotmail.fr>
2023-09-22 11:26:40 +0100

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# create a file in /tmp
-
+# Creates a file
 file { '/tmp/school':
-  ensure   => 'file',
-  content  => 'I love Puppet',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
