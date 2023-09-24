ca49ad4fb5119d945f285ce456842ffdd4716eb9
alijr2018 <alirami120@hotmail.fr>
2023-09-25 00:54:16 +0100

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@
 file_line { 'Declare identity file':
 ensure => 'present',
 path   => '/etc/ssh/ssh_config',
-line   => '    IdentityFile ~/.ssh/school',
+line   => 'IdentityFile ~/.ssh/school'
 }
 
 file_line { 'Turn off passwd auth':
