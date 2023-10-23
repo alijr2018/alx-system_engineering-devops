79ca7c63d5888b962c4623401d754d3ef6c339ea
alijr2018 <alijr_2018@proton.me>
2023-10-23 12:10:26 +0200

    done

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress
-and export data in the CSV format.
+Python script to export data in the CSV format.
 """
 
 import csv
