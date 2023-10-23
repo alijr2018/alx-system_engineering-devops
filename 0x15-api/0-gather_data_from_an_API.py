ec1e2df1af1c6cc29c3fea9542f78c0982bce0ba
alijr2018 <alijr_2018@proton.me>
2023-10-23 11:41:22 +0200

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 """a Python script that, using this REST API, for a given employee
 ID, returns information about his/her TODO list progress."""
+
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-def get_employee_todo_progress(employee_id):
-    emp_req = requests.get(f"{REST_API}/users/{employee_id}").json()
-    task_req = requests.get(f"{REST_API}/todos?userId={employee_id}").json()
-    emp_name = emp_req.get('name')
-    total_tasks = len(task_req)
-    completed_tasks = [task for task in task_req if task.get('completed')]
+def check_tasks(filename):
+    """ Fetch user name, number of tasks """
 
-    print(
-        f'Employee {emp_name} is done with tasks({len(completed_tasks)}/{total_tasks}):'
-    )
+    resp = requests.get(todos_url).json()
 
-    for task in completed_tasks:
-        print(f'\t{task.get("title")}')
+    count = 0
+    with open(filename, 'r') as f:
+        next(f)  # Skip the first line
+        for line in f:
+            count += 1
+            if line.startswith('\t '):
+                print(f"Task {count} Formatting: OK")
+            else:
+                print(f"Task {count} Formatting: Incorrect")
 
-if __name__ == '__main__':
+if __name__ == "__main":
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 main.py <student_output_file>")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    check_tasks(sys.argv[1])
