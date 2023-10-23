0662b8fbaef3e3d023a83011185d8f110cce7121
alijr2018 <alijr_2018@proton.me>
2023-10-23 09:07:28 +0200

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,30 +8,34 @@ import sys
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
 
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data["name"]
+    try:
+        user_response = requests.get(user_url)
+        todo_response = requests.get(todo_url)
 
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
+        user_data = user_response.json()
+        todo_data = todo_response.json()
 
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo["completed"])
+        employee_name = user_data.get('name')
+        total_tasks = len(todo_data)
+        done_tasks = len([task for task in todo_data if task['completed']])
 
-    print(f"Employee {employee_name} is done with tasks"
-          f"({completed_tasks}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks"
+              f"({done_tasks}/{total_tasks}):")
+        for task in todo_data:
+            if task['completed']:
+                print(f"\t{task['title']}")
 
-    for todo in todos_data:
-        if todo["completed"]:
-            print(f"    {todo['title']}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-
     if len(sys.argv) != 2:
         print("Usage: python3 gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
