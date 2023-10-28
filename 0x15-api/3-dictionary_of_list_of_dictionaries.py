d314a5247826ea908b2686c861f6e3b627f2bacf
alijr2018 <alijr_2018@proton.me>
2023-10-28 11:20:13 +0200

    done

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """ Python script to export data in the JSON format. """
+import csv
 import json
 import requests
 
-if __name__ == '__main':
+if __name__ == '__main__':
+    employee_id = 1
     user_tasks = {}
     url_todo = 'https://jsonplaceholder.typicode.com/todos/'
     url_user = 'https://jsonplaceholder.typicode.com/users/'
     users = requests.get(url_user).json()
 
-    for user in users:
-        employee_id = user['id']
+    for employee_id in range(1, len(users) + 1):
         todo = requests.get(url_todo, params={'userId': employee_id})
+        user = requests.get(url_user, params={'id': employee_id})
 
         todo_dict_list = todo.json()
+        user_dict_list = user.json()
         task_list = []
-        employee = user['username']
+        employee = user_dict_list[0].get('username')
 
         for task in todo_dict_list:
             status = task.get('completed')
@@ -27,5 +30,5 @@ if __name__ == '__main':
             task_list.append(task_dict)
         user_tasks[employee_id] = task_list
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(user_tasks, jsonfile, indent=4)
+    with open("todo_all_employees.json", "w+") as jsonfile:
+        json.dump(user_tasks, jsonfile)
