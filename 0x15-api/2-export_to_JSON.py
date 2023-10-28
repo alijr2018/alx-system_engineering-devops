4855024f708a03a83706560710f8821b150ea6d9
alijr2018 <alijr_2018@proton.me>
2023-10-28 11:13:58 +0200

    done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,25 +8,28 @@ from sys import argv
 url_todo = 'https://jsonplaceholder.typicode.com/todos/'
 url_user = 'https://jsonplaceholder.typicode.com/users/'
 
-if __name__ == '__main':
+
+if __name__ == '__main__':
     employee_id = argv[1]
     todo = requests.get(url_todo, params={'userId': employee_id})
     user = requests.get(url_user, params={'id': employee_id})
 
     todo_dict_list = todo.json()
     user_dict_list = user.json()
+    task_list = []
     user_tasks = {}
     employee = user_dict_list[0].get('username')
 
-    for task in todo_dict_list:
-        status = task.get('completed')
-        title = task.get('title')
-        task_dict = {}
-        task_dict['task'] = title
-        task_dict['completed'] = status
-        task_dict['username'] = employee
-        user_tasks[employee_id] = user_tasks.get(employee_id, [])
-        user_tasks[employee_id].append(task_dict)
-
     with open("{}.json".format(employee_id), "w+") as jsonfile:
-        json.dump(user_tasks, jsonfile, indent=4)
+        for task in todo_dict_list:
+            status = task.get('completed')
+            title = task.get('title')
+            task_dict = {}
+            task_dict['task'] = title
+            task_dict['completed'] = status
+            task_dict['username'] = employee
+            task_list.append(task_dict)
+        user_tasks[employee_id] = task_list
+
+        data = json.dumps(user_tasks)
+        jsonfile.write(data)
