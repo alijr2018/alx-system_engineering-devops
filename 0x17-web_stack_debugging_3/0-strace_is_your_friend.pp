71905dba1782e2174ff7dc2c106d01bce8f45c58
alijr2018 <alijr_2018@proton.me>
2023-11-08 19:29:15 +0100

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet 
 
-file { 'fix':
-ensure  => file,
-content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
 }
