b23906f95371649360f9948441eeb12dfd9fd8ee
alijr2018 <alijr_2018@proton.me>
2023-11-08 19:32:05 +0100

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 
 file { '/var/www/html/wp-settings.php':
   ensure  => file,
-  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php'),
+  content => file('/var/www/html/wp-settings.php').content.gsub('phpp', 'php')
 }
 
