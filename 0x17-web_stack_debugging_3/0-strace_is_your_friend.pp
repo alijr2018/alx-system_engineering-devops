4e2d16f6f052c29a365dd52e0b394697caf8f1bd
alijr2018 <alijr_2018@proton.me>
2023-11-08 19:34:50 +0100

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-#find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet 
+# Fixing why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet
 
 exec { 'fix':
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
