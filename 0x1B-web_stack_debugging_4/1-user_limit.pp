395a0f51138c557ce21fb793e7d13a36c4566df7
alijr2018 <alijr_2018@proton.me>
2023-11-16 20:44:36 +0100

    done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,7 @@
 # Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
 
-class user_limits {
-
-  exec { 'set_holberton_limits':
-    command => '/bin/su - holberton -c "ulimit -n 65535"',
-    path    => '/bin:/usr/bin',
-  }
+exec { 'change_limits_conf':
+  command => '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin:/bin',
 }
-
-class { 'user_limits': }
 
