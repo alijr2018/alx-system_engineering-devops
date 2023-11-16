00207ccaf58a8d9b39af8285d8a340298d946106
alijr2018 <alijr_2018@proton.me>
2023-11-16 19:56:39 +0100

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,19 +1,15 @@
-# Description: Puppet manifest to modify Nginx configuration and restart service
-
-# Update the configuration in /etc/default/nginx
+# A benchmark for web server setup featuring Nginx is doing under pressure
 exec { 'modify_nginx_config':
   command => '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx',
   path    => '/bin:/usr/bin',
   notify  => Exec['restart_nginx'],
 }
 
-# Restart the Nginx service after modifying the config
 exec { 'restart_nginx':
   command     => '/usr/bin/env service nginx restart',
   path        => '/sbin:/usr/sbin:/bin:/usr/bin',
   refreshonly => true,
 }
 
-# Define relationships between tasks
 Exec['modify_nginx_config'] -> Exec['restart_nginx']
 
